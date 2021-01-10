b1c5d33cce9470f4ce5da7404eb5a5e2c0608d02
ashikkhandani <mdashikahammed@gmail.com>
2021-01-10 16:29:08 +0100

    modified

--- script.js
+++ script.js
@@ -1,192 +1,193 @@
 "use strict";
+window.onload = () => {
+  let rotateMsg = alert("Please rotate your mobile device to play the Game.");
 
-let rotateMsg = alert("Please rotate your mobile device to play the Game.");
+  // Selecting Elements
+  const player0El = document.querySelector(".player--0");
+  const player1El = document.querySelector(".player--1");
+  const score0El = document.querySelector("#score--0");
+  const score1El = document.getElementById("score--1");
+  const current0El = document.getElementById("current--0");
+  const current1El = document.getElementById("current--1");
+  const winMsg = document.querySelector(".message");
 
-// Selecting Elements
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
-const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1");
-const current0El = document.getElementById("current--0");
-const current1El = document.getElementById("current--1");
-const winMsg = document.querySelector(".message");
+  const diceEl = document.querySelector(".dice");
+  const btnNew = document.querySelector(".btn--new");
+  const btnRoll = document.querySelector(".btn--roll");
+  const btnHold = document.querySelector(".btn--hold");
+  const btnCloseModal = document.querySelector(".close-modal");
+  const modal = document.querySelector(".modal");
+  const overlay = document.querySelector(".overlay");
 
-const diceEl = document.querySelector(".dice");
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
-const btnCloseModal = document.querySelector(".close-modal");
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
+  let scores, currentScore, activePlayer, playing;
 
-let scores, currentScore, activePlayer, playing;
+  // create dice Roll Sound
+  let diceSound = document.createElement("audio");
+  diceSound.src = "./sounds/dice.mp3";
+  diceSound.setAttribute("controls", "none");
+  diceSound.setAttribute("preload", "auto");
+  diceSound.style.display = "none";
+  diceSound.volume = 0.2;
+  container.append(diceSound);
 
-// create dice Roll Sound
-let diceSound = document.createElement("audio");
-diceSound.src = "./sounds/dice.mp3";
-diceSound.setAttribute("controls", "none");
-diceSound.setAttribute("preload", "auto");
-diceSound.style.display = "none";
-diceSound.volume = 0.5;
-container.append(diceSound);
+  // create hold Sound
+  let holdSound = document.createElement("audio");
+  holdSound.src = "./sounds/hold.mp3";
+  holdSound.setAttribute("controls", "none");
+  holdSound.setAttribute("preload", "auto");
+  holdSound.style.display = "none";
+  holdSound.volume = 0.1;
+  container.append(holdSound);
 
-// create hold Sound
-let holdSound = document.createElement("audio");
-holdSound.src = "./sounds/hold.mp3";
-holdSound.setAttribute("controls", "none");
-holdSound.setAttribute("preload", "auto");
-holdSound.style.display = "none";
-holdSound.volume = 0.3;
-container.append(holdSound);
+  // create game sound
+  let gameSound = document.createElement("audio");
+  gameSound.src = "./sounds/gamefull.mp3";
+  gameSound.setAttribute("controls", "none");
+  gameSound.setAttribute("preload", "auto");
+  gameSound.style.display = "none";
+  gameSound.volume = 0.05;
+  gameSound.loop = true;
+  container.append(gameSound);
+  gameSound.play();
 
-// create game sound
-let gameSound = document.createElement("audio");
-gameSound.src = "./sounds/gamefull.mp3";
-gameSound.setAttribute("controls", "none");
-gameSound.setAttribute("preload", "auto");
-gameSound.style.display = "none";
-gameSound.volume = 0.2;
-gameSound.loop = true;
-container.append(gameSound);
-gameSound.play();
+  // create victory sound
+  let victorySound = document.createElement("audio");
+  victorySound.src = "./sounds/victory.mp3";
+  victorySound.setAttribute("controls", "none");
+  victorySound.setAttribute("preload", "auto");
+  victorySound.style.display = "none";
+  victorySound.volume = 0.2;
+  victorySound.loop = true;
+  container.append(victorySound);
 
-// create victory sound
-let victorySound = document.createElement("audio");
-victorySound.src = "./sounds/victory.mp3";
-victorySound.setAttribute("controls", "none");
-victorySound.setAttribute("preload", "auto");
-victorySound.style.display = "none";
-victorySound.volume = 0.5;
-victorySound.loop = true;
-container.append(victorySound);
+  // Modal functionality
+  const showModal = function () {
+    modal.classList.remove("hidden1");
+    overlay.classList.remove("hidden1");
+    victorySound.play();
+    victorySound.volume = 0.2;
+  };
 
-// Modal functionality
-const showModal = function () {
-  modal.classList.remove("hidden1");
-  overlay.classList.remove("hidden1");
-  victorySound.play();
-  victorySound.volume = 0.5;
-};
+  const closeModal = function () {
+    modal.classList.add("hidden1");
+    overlay.classList.add("hidden1");
+    victorySound.volume = 0;
+    //   gameSound.volume = 0.2;
+  };
 
-const closeModal = function () {
-  modal.classList.add("hidden1");
-  overlay.classList.add("hidden1");
-  victorySound.volume = 0;
-  gameSound.volume = 0.2;
-};
+  // Starting Condition
+  const init = function () {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-// Starting Condition
-const init = function () {
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
 
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
+    diceEl.classList.add("hidden");
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+    gameSound.volume = 0.2;
+  };
+  init();
 
-  diceEl.classList.add("hidden");
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
-  gameSound.volume = 0.2;
-};
-init();
+  const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+  };
 
-const switchPlayer = function () {
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle("player--active");
-  player1El.classList.toggle("player--active");
-};
+  // Rolling Dice functionality
+  btnRoll.addEventListener("click", function () {
+    if (playing) {
+      // game sound play
+      gameSound.play();
 
-// Rolling Dice functionality
-btnRoll.addEventListener("click", function () {
-  if (playing) {
-    // game sound play
-    gameSound.play();
+      // dice sound play
+      diceSound.play();
 
-    // dice sound play
-    diceSound.play();
+      //1. Generating a random dice roll
+      const dice = Math.trunc(Math.random() * 6) + 1;
+      console.log(dice);
 
-    //1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
+      //2. Display dice
+      diceEl.classList.remove("hidden");
+      diceEl.src = `dice-${dice}.png`;
 
-    //2. Display dice
-    diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${dice}.png`;
+      //3. Check for rolled 1
+      if (dice !== 1) {
+        // Add dice to current score
+        currentScore += dice;
+        document.getElementById(
+          `current--${activePlayer}`
+        ).textContent = currentScore;
+      } else {
+        holdSound.volume = 0.3;
 
-    //3. Check for rolled 1
-    if (dice !== 1) {
-      // Add dice to current score
-      currentScore += dice;
-      document.getElementById(
-        `current--${activePlayer}`
-      ).textContent = currentScore;
-    } else {
-      holdSound.volume = 0.3;
-
-      // switch to next player
-      switchPlayer();
+        // switch to next player
+        switchPlayer();
+      }
     }
-  }
-});
+  });
 
-// Hold button functionality
-btnHold.addEventListener("click", function () {
-  if (playing) {
-    // hold sound play
-    holdSound.play();
-    //1. Add current score to active player's score
-    scores[activePlayer] += currentScore;
-    // console.log(scores[activePlayer]);
+  // Hold button functionality
+  btnHold.addEventListener("click", function () {
+    if (playing) {
+      // hold sound play
+      holdSound.play();
+      //1. Add current score to active player's score
+      scores[activePlayer] += currentScore;
+      // console.log(scores[activePlayer]);
 
-    document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
+      document.getElementById(`score--${activePlayer}`).textContent =
+        scores[activePlayer];
 
-    //2. Check if player's score is >=500
-    if (scores[activePlayer] >= 10) {
-      // Finish the Game
-      playing = false;
+      //2. Check if player's score is >=500
+      if (scores[activePlayer] >= 10) {
+        // Finish the Game
+        playing = false;
 
-      showModal();
-      holdSound.volume = 0;
+        showModal();
+        holdSound.volume = 0;
 
-      victorySound.play();
-      gameSound.volume = 0;
+        victorySound.play();
+        gameSound.volume = 0;
 
-      btnCloseModal.addEventListener("click", closeModal);
+        btnCloseModal.addEventListener("click", closeModal);
 
-      overlay.addEventListener("click", closeModal);
+        overlay.addEventListener("click", closeModal);
 
-      diceEl.classList.add("hidden");
+        diceEl.classList.add("hidden");
 
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
+        document
+          .querySelector(`.player--${activePlayer}`)
+          .classList.add("player--winner");
 
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
-    } else {
-      holdSound.volume = 0.3;
+        document
+          .querySelector(`.player--${activePlayer}`)
+          .classList.remove("player--active");
+      } else {
+        holdSound.volume = 0.3;
 
-      // Switch to the next player
-      switchPlayer();
+        // Switch to the next player
+        switchPlayer();
+      }
     }
-  }
-});
-// Restore everything
-btnNew.addEventListener("click", init);
+  });
+  // Restore everything
+  btnNew.addEventListener("click", init);
 
-// to close modal with Escape key
-document.addEventListener("keydown", function (escape) {
-  if (escape.key === "Escape" && !modal.classList.contains("hidden1")) {
-    closeModal();
-  }
-});
+  // to close modal with Escape key
+  document.addEventListener("keydown", function (escape) {
+    if (escape.key === "Escape" && !modal.classList.contains("hidden1")) {
+      closeModal();
+    }
+  });
+};
